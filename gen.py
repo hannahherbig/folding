1e28874f0caa4c9eadb5088dd1bc1dd27e771eb6
Andrew Herbig <notandrewherbig@gmail.com>
2020-12-09 10:30:11 -0500

    write out txt files with tables

--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 import subprocess
-from datetime import datetime
+from datetime import datetime, timedelta
+from pathlib import Path
 from typing import Optional
 
 import git
@@ -18,28 +19,36 @@ def git_commits_for(path):
 
 
 repo = git.Repo(".", odbt=git.db.GitCmdObjectDB)
-name = "data/donor/andrew12.json"
-commits = git_commits_for(name)
 
 
-class Item(BaseModel):
-    last: datetime
-    credit: int
-    wus: int
-    credit_change: Optional[int] = None
-    wus_change: Optional[int] = None
+def write_table(name):
+    commits = git_commits_for(name)
+
+    class Item(BaseModel):
+        last: datetime
+        credit: int
+        wus: int
+        last_change: Optional[timedelta] = None
+        credit_change: Optional[int] = None
+        wus_change: Optional[int] = None
+        ppd: Optional[int] = None
+
+    rows = {}
+    for ref in commits:
+        blob = repo.commit(ref).tree[name].data_stream.read()
+        i = Item.parse_raw(blob)
+        rows[i.last] = i
+
+    table = []
+    for a, b in pairwise(sorted(rows.values(), key=lambda i: i.last, reverse=True)):
+        a.last_change = a.last - b.last
+        a.credit_change = a.credit - b.credit
+        a.wus_change = a.wus - b.wus
+        a.ppd = int(a.credit_change / a.last_change.total_seconds() * 86400)
+        table.append(a.dict())
+
+    Path(name).with_suffix(".txt").write_text(tabulate(table, headers="keys") + "\n")
 
 
-rows = {}
-for ref in commits:
-    blob = repo.commit(ref).tree[name].data_stream.read()
-    i = Item.parse_raw(blob)
-    rows[i.last] = i
-
-table = []
-for a, b in pairwise(sorted(rows.values(), key=lambda i: i.last, reverse=True)):
-    a.credit_change = a.credit - b.credit
-    a.wus_change = a.wus - b.wus
-    table.append(a.dict())
-
-print(tabulate(table, headers="keys"))
+write_table("data/donor/andrew12.json")
+write_table("data/team/45032.json")
