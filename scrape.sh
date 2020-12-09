6195154999a62db64a740aa6ad234062088a52fd
Andrew Herbig <notandrewherbig@gmail.com>
2020-12-09 00:32:14 -0500

    add monthly files

--- scrape.sh
+++ scrape.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -ex
 
 paths=(
     donors
     teams
+    donors-monthly
+    teams-monthly
     donor/andrew12
     team/45032
 )
@@ -13,5 +15,5 @@ for path in ${paths[@]}
 do
     out=data/$path
     mkdir -p $(dirname $out)
-    curl https://stats.foldingathome.org/api/$path > $out.json
+    curl -s https://stats.foldingathome.org/api/$path -o $out.json
 done
