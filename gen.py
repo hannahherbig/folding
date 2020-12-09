6c2df51e2393e92aef93da160e8ac539d04e4e84
Andrew Herbig <notandrewherbig@gmail.com>
2020-12-09 03:17:14 -0500

    do a table pairwise

--- gen.py
+++ gen.py
@@ -1,7 +1,11 @@
-import json
 import subprocess
+from datetime import datetime
+from typing import Optional
 
 import git
+from more_itertools import pairwise
+from pydantic import BaseModel
+from tabulate import tabulate
 
 
 def git_commits_for(path):
@@ -14,10 +18,28 @@ def git_commits_for(path):
 
 
 repo = git.Repo(".", odbt=git.db.GitCmdObjectDB)
-name = "data/team/45032.json"
+name = "data/donor/andrew12.json"
 commits = git_commits_for(name)
 
+
+class Item(BaseModel):
+    last: datetime
+    credit: int
+    wus: int
+    credit_change: Optional[int] = None
+    wus_change: Optional[int] = None
+
+
+rows = {}
 for ref in commits:
     blob = repo.commit(ref).tree[name].data_stream.read()
-    donor = json.loads(blob)
-    print(donor["last"], donor["credit"], donor["wus"])
+    i = Item.parse_raw(blob)
+    rows[i.last] = i
+
+table = []
+for a, b in pairwise(sorted(rows.values(), key=lambda i: i.last, reverse=True)):
+    a.credit_change = a.credit - b.credit
+    a.wus_change = a.wus - b.wus
+    table.append(a.dict())
+
+print(tabulate(table, headers="keys"))
